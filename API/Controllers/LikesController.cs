6badb09ea6641b007b81923da7f8b16d665d28d3
matthew-njd <mnjdagostino@gmail.com>
2025-01-22 10:04:42 -0500

    Likes Controller added

---
+++ API/Controllers/LikesController.cs
@@ -0,0 +1,55 @@
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace API.Controllers
+{
+    public class LikesController(ILikesRepository likesRepository) : BaseApiController
+    {
+        [HttpPost("{targetUserId:int}")]
+        public async Task<ActionResult> ToggleLike(int targetUserId)
+        {
+            var sourceUserId = User.GetUserId();
+
+            if (sourceUserId == targetUserId) return BadRequest("You cannot like yourself");
+
+            var existingLke = await likesRepository.GetUserLike(sourceUserId, targetUserId);
+
+            if (existingLke == null)
+            {
+                var like = new UserLike
+                {
+                    SourceUserId = sourceUserId,
+                    TargetUserId = targetUserId
+                };
+
+                likesRepository.AddLike(like);
+            }
+            else 
+            {
+                likesRepository.DeleteLike(existingLke);
+            }
+
+            if (await likesRepository.SaveChnages()) return Ok();
+
+            return BadRequest("Failed to update like");
+        }
+
+        [HttpGet("list")]
+        public async Task<ActionResult<IEnumerable<int>>> GetCurrentUserLikeIds() 
+        {
+            return Ok(await likesRepository.GetCurrentUserLikeId(User.GetUserId()));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUserLikes(string predicate)
+        {
+            var users = await likesRepository.GetUserLikes(predicate, User.GetUserId());
+            
+            return Ok(users);
+        }
+    }
+}
