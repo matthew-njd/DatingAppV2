debf463b7cafedbce369fd0c698dc4188e935b20
matthew-njd <mnjdagostino@gmail.com>
2025-01-27 17:52:09 -0500

    Messaging Entity & Repo init

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -18,6 +18,8 @@ namespace API.Entities
         public required string Country { get; set; }
         public List<Photo> Photos { get; set; } = [];
         public List<UserLike> LikedByUsers { get; set; } = [];
-        public List<UserLike> LikedUsers { get; set; } = [];        
+        public List<UserLike> LikedUsers { get; set; } = [];
+        public List<Message> MessagesSent { get; set; } = [];
+        public List<Message> MessagesReceived { get; set; } =[];
     }
 }
--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -10,12 +10,15 @@ namespace API.Helpers
         public AutoMapperProfiles()
         {
             CreateMap<AppUser, MemberDto>()
-            .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
-            .ForMember(d => d.PhotoUrl, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
+                .ForMember(d => d.Age, o => o.MapFrom(s => s.DateOfBirth.CalculateAge()))
+                .ForMember(d => d.PhotoUrl, o => o.MapFrom(s => s.Photos.FirstOrDefault(x => x.IsMain)!.Url));
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDto, AppUser>();
             CreateMap<RegisterDto, AppUser>();
             CreateMap<string, DateOnly>().ConvertUsing(s => DateOnly.Parse(s));
+            CreateMap<Message, MessageDto>()
+                .ForMember(d => d.SenderPhotoUrl, o => o.MapFrom(s => s.Sender.Photos.FirstOrDefault(x => x.IsMain)!.Url))
+                .ForMember(d => d.SenderPhotoUrl, o => o.MapFrom(s => s.Recipient.Photos.FirstOrDefault(x => x.IsMain)!.Url));
         }
     }
 }
