8c537d731f1fac154d608f223665d9a21c5f5f94
Matthew D'Agostino <mnjdagostino@gmail.com>
2025-04-28 19:53:27 -0400

    identity and role init refactoring

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -22,8 +22,6 @@ namespace API.Controllers
             var user = mapper.Map<AppUser>(regitsterDto);
 
             user.UserName = regitsterDto.Username.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(regitsterDto.Password));
-            user.PasswordSalt = hmac.Key;
 
             context.Users.Add(user);
             await context.SaveChangesAsync();
@@ -43,15 +41,6 @@ namespace API.Controllers
             var user = await context.Users.Include(p => p.Photos).FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
             if (user == null) return Unauthorized("Invalid username");
-
-            using var hmac = new HMACSHA512(user.PasswordSalt);
-
-            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-            for (int i = 0; i < computedHash.Length; i++)
-            {
-                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-            }
 
             return new UserDto
             {
